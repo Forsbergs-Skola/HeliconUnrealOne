42e1a394f8e341b11ee8c9fb810fa6ee6971c308
Olle Salin <olle.salin@student.forsbergsskola.se>
2026-06-30 09:22:41 +0200

    TutorialPuzzle

--- HeliconUnreal/Source/HeliconUnreal/Private/PuzzleManager.cpp
+++ HeliconUnreal/Source/HeliconUnreal/Private/PuzzleManager.cpp
@@ -19,3 +19,29 @@ void APuzzleManager::ToggleRoomState()
 	
 	OnRoomStateChanged.Broadcast(bStateB);
 }
+
+void APuzzleManager::RegisterActivatedObject()
+{
+	ActivatedObjects++;
+	
+	GEngine->AddOnScreenDebugMessage(
+	-1,
+	2.f,
+	FColor::Yellow,
+	FString::Printf(TEXT("%d / %d"),
+	ActivatedObjects,
+	RequiredObjects)
+);
+
+	if (ActivatedObjects >= RequiredObjects)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			2.f,
+			FColor::Green,
+			TEXT("Puzzle Unlocked!")
+		);
+
+		OnPuzzleUnlocked.Broadcast();
+	}
+}
